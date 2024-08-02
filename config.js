d7753f6d52bd878a61556e837a94d4579eb6f13f
Dx-Hacking108 <118250883+Dx-Hacking108@users.noreply.github.com>
2024-08-02 14:58:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
 global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/EqAn0oRn8tYFKzSM62odvh";
 global.website=process.env.GURL || "https://chat.whatsapp.com/EqAn0oRn8tYFKzSM62odvh" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://github.com/SuhailTechInfo/Suhail-Md/blob/main/lib/assets/suhail.jpg?raw=true" ; // SET LOGO FOR IMAGE 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/Dx-Hacking108/Suhail-Md/main/20230926_014117.jpg?raw=true" ; // SET LOGO FOR IMAGE 
 global.caption = process.env.CAPTION || global.caption || "〄ＤＸ３６９ム" 
 
 
